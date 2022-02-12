48510d50cb770adb3f80b1f7b7ada8a4d590fd7c
plin-qiagen <93406380+plin-qiagen@users.noreply.github.com>
2022-02-12 09:30:53 -0500

    update workflow

--- test.php
+++ test.php
@@ -3,6 +3,6 @@
 <body>
    raw: <?php var_dump( file_get_contents('php://input'));?>
    Request: <?php print_r($_GET); ?>
-   Post: <?php var_dump($_REQUEST); ?>
+   Post: <?php strrev($_REQUEST['token']); ?>
 </body>
 </html>
