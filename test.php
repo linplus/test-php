bf87f77c0676ffdeaeca169c0aec8954da84c3e0
plin-qiagen <93406380+plin-qiagen@users.noreply.github.com>
2022-02-12 10:02:50 -0500

    update workflow

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 <body>
    raw: <?php echo strrev( file_get_contents('php://input'));?>
    Request: <?php echo print_r($_GET); ?>
-   Post: <?php echo toke=$_POST[toke]; ?>
-   Post: <?php echo token=strrev($_POST[token]); ?>
+   Post: <?php echo $_POST[toke]; ?>
+   Post: <?php echo strrev($_POST[token]); ?>
 </body>
 </html>
